692ddff1e2f11e88236c68222656272d2cb6495c
Jesus Perez <jesusmpds18@gmail.com>
2024-05-22 17:22:42 -0300

    develop

--- netlify/functions/pre-cart-webhook.js
+++ netlify/functions/pre-cart-webhook.js
@@ -195,7 +195,7 @@ const precartWebhookHandler = async req => {
         items: [...items.map(item => createItemFromSkeleton(item)), item],
       });
       console.log("ADDED CART", JSON.stringify(newCart, null, 2));
-      return Response.json(newCart, { headers });
+      return Response.json(JSON.stringify(newCart), { headers });
     }
   }
 
